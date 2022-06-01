d1c5478a2f593ec54c5c91ff16771ee5b30d08ef
iram96 <mariano.maselli@virgilio.it>
2022-06-01 20:54:03 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,29 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        //List<Pizza> pizzaList = new List<Pizza>();
+        public static pizzaList pizze;
+        public IActionResult Pizzeria()
+        {
+            pizze = new pizzaList();
+
+
+            Pizza Pizza1 = new Pizza("Margherita", "Pomodoro Mozzarella", 10);
+
+            Pizza Pizza2 = new Pizza("Marinara", "Pomodoro Origano", 10);
+
+            Pizza Pizza3 = new Pizza("Diavola", "Pomodoro Mozzarella Salame piccante", 15);
+
+
+            pizze.ListaPizze.Add(Pizza1);
+            pizze.ListaPizze.Add(Pizza2);
+            pizze.ListaPizze.Add(Pizza3);
+
+            return View(pizze);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,29 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+
+        public string Desc { get; set; }
+
+        public int Prezzo { get; set; }
+        public Pizza(string Nome, string Desc, int Prezzo)
+        {
+            this.Nome = Nome;
+            this.Desc = Desc;
+            this.Prezzo = Prezzo;
+        }
+
+    }
+
+    public class pizzaList
+    {
+        public List<Pizza> ListaPizze { get; set; }
+
+        public pizzaList()
+        {
+            ListaPizze = new();
+
+        }
+    }
+}
